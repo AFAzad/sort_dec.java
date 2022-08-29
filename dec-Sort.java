71e8e54235729c871529cd1e4cc03cc34ad804e3
Abul Fajle Azad <abulfajle405@gmail.com>
2022-08-29 21:51:03 +0530

    Initial commit

---
+++ dec-Sort.java
@@ -0,0 +1,35 @@
+//Bubble Sort (descending order)
+
+//Q. Given an array, sort the array in reverse order by simply swapping its adjacent elements.
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int N= sc.nextInt();
+		int[] arr= new int[N];
+		for(int i=0; i<N; i++)
+		{
+			arr[i]= sc.nextInt();
+		}      
+		for(int i=0; i<N; i++)
+		{
+			for(int j=0; j<N-1;j++)
+			{
+				if(arr[j]<arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		    for(int i=0; i<N; i++)
+			{
+				System.out.print(arr[i]+" ");
+			}    // Your code here
+	}
+}
